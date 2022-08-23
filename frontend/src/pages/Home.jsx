308d1d480d9ad347d0df7c52e7b90a337ef247bc
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-08-23 02:30:14 -0400

    :

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="grid_name">
+        <h1 className="MyName">
+          <span className="span1">My name is,</span>Jose Portillo.
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
