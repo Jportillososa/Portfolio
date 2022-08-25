d121d203033db613d21cddac9866b5191dd4a0d4
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-08-25 13:57:46 -0400

    styled projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -11,105 +11,76 @@ const Projects = () => {
       </h2>
 
       <div className="project">
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
+        <div className="project_card">
+          <img
+            className="project_img"
+            src="/images/FatCatGuitars.jpeg"
+            alt="website"
+          />
+          <h4 className="project-name">FatCat Guitars</h4>
           <button>Github</button>
           <button>Visit Site</button>
+        </div>
+        <div className="project_card">
+          <div className="product-info">
+            <img
+              className="project_img"
+              src="/images/ComplexRedesign.jpeg"
+              alt="website"
+            />
+            <h4 className="project-name">Complex Redesign</h4>
+            <button>Github</button>
+            <button>Visit Site</button>
+          </div>
         </div>
 
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
+        <div className="project_card">
+          <div className="product-info">
+            <img
+              className="project_img"
+              src="/images/Learnify.jpeg"
+              alt="website"
+            />
+            <h4 className="project-name">Learnify</h4>
+            <button>Github</button>
+            <button>Visit Site</button>
+          </div>
         </div>
-
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
+        <div className="project_card">
+          <div className="product-info">
+            <img
+              className="project_img"
+              src="/images/HopeHacks.jpeg"
+              alt="website"
+            />
+            <h4 className="project-name">STAY (Code for Justice)</h4>
+            <button>Github</button>
+            <button>Visit Site</button>
+          </div>
         </div>
-
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
+        <div className="project_card">
+          <div className="product-info">
+            <img
+              className="project_img"
+              src="/images/TaskTracker.jpeg"
+              alt="website"
+            />
+            <h4 className="project-name">Task Tracker!</h4>
+            <button>Github</button>
+            <button>Visit Site</button>
+          </div>
         </div>
-
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
-        </div>
-        <div className="product-info">
-          <h4 className="product-price">PROJECT #1</h4>
-          <button>Github</button>
-          <button>Visit Site</button>
+        <div className="project_card">
+          <div className="product-info">
+            <img
+              className="project_img"
+              src="/images/GatewayBali.jpeg"
+              alt="website"
+            />
+            <h4 className="project-name">Visit Bali!</h4>
+            <button>Github</button>
+            <button>Visit Site</button>
+          </div>
         </div>
       </div>
     </div>
